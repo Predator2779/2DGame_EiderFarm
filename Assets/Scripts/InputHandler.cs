e4e98f622f588da168181c3bbb51030151c604ec
Predator2779 <predator2779@gmail.com>
2023-11-12 23:13:20 +0500

    fixed IMovable

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -1,23 +1,21 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Person))]
+[RequireComponent(typeof(IMovable))]
 public class InputHandler : MonoBehaviour
 {
-    [SerializeField] private Person _person;
     [SerializeField] private Joystick _joystick;
 
     [Header("Joystick sensetivity.")]
     [SerializeField, Range(0, 1)] private float _sensitivity;
 
-    private void Start()
-    {
-        _person = GetComponent<Person>();
-    }
+    private IMovable _movable;
+
+    private void Start() => _movable = GetComponent<IMovable>();
 
     private void Update()
     {
         Vector3 direction = new Vector3(_joystick.Horizontal, _joystick.Vertical, 0);
 
-        if (direction.magnitude > _sensitivity) _person.Move(direction.normalized);
+        if (direction.magnitude > _sensitivity) _movable.Move(direction.normalized);
     }
 }
