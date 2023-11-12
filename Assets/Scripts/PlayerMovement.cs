556192c7b0a8927a685780029bbb33a02414ac02
Predator2779 <predator2779@gmail.com>
2023-11-12 20:35:18 +0500

    fixed colliders and movement
    пофиксил прохождение через коллайдеры (вроде бы). Слегка изменил скрипт перемещения

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -7,7 +6,16 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Joystick _joystick;
 
     [Header("Скорость движения персонажа.")]
-    [SerializeField, Range(0, 100)] private float _speed;
+    [SerializeField, Range(0, 10)] private float _speed;
+    [Header("Коэффициент скорости.")]
+    [SerializeField] private float _coefSpeed;   
+    [Header("Чувствительность стика.")]
+    [SerializeField, Range(0, 1)] private float _sensitivity;
+
+    private void Start()
+    {
+        throw new NotImplementedException();
+    }
 
     private void Update()
     {
@@ -18,14 +26,10 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector3 direction = new Vector3(_joystick.Horizontal, _joystick.Vertical, 0);
         
-        if(direction.magnitude > 0.1f)
+        if(direction.magnitude > _sensitivity)
         {
-            Vector3 movement = direction.normalized * _speed * Time.deltaTime;
+            Vector3 movement = direction.normalized * _speed * _coefSpeed * Time.deltaTime;
             transform.Translate(movement);
         }
     }
 }
-
-
-
-
