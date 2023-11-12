c3ba8c1ea23c6cf0b39fcb7238852f6dcd2293bd
Predator2779 <predator2779@gmail.com>
2023-11-12 20:50:53 +0500

    fixed colliders-2

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -7,28 +6,18 @@ public class PlayerMovement : MonoBehaviour
 
     [Header("Скорость движения персонажа.")]
     [SerializeField, Range(0, 10)] private float _speed;
-    [Header("Коэффициент скорости.")]
-    [SerializeField] private float _coefSpeed;   
     [Header("Чувствительность стика.")]
     [SerializeField, Range(0, 1)] private float _sensitivity;
 
-    private void Start()
-    {
-        throw new NotImplementedException();
-    }
-
-    private void Update()
-    {
-        Move();
-    }
+    private void Update() => Move();
 
     private void Move()
     {
         Vector3 direction = new Vector3(_joystick.Horizontal, _joystick.Vertical, 0);
-        
-        if(direction.magnitude > _sensitivity)
+
+        if (direction.magnitude > _sensitivity)
         {
-            Vector3 movement = direction.normalized * _speed * _coefSpeed * Time.deltaTime;
+            Vector3 movement = direction.normalized * _speed * Time.deltaTime;
             transform.Translate(movement);
         }
     }
