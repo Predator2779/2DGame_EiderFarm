576161722ec5900187e6283e92b660a3a9f9a820
Predator2779 <predator2779@gmail.com>
2023-11-13 23:39:41 +0500

    added turn to person

--- Assets/Scripts/Characters/Person.cs
+++ Assets/Scripts/Characters/Person.cs
@@ -1,3 +1,5 @@
+using System;
+using Player;
 using UnityEngine;
 
 [RequireComponent(typeof(Movement))]
@@ -5,10 +7,31 @@ public class Person : MonoBehaviour, IMovable
 {
     [Header("Character speed.")]
     [SerializeField, Range(0, 100)] private float _speed;
-    
+
+    [SerializeField] private SpriteRenderer _renderer;
+
     private Movement _movement;
+    private TurnHandler _turnHandler;
 
-    private void Start() => _movement = GetComponent<Movement>();
+    private void OnValidate() => SetNullableFields();
+    private void Start() => SetNullableFields();
 
-    public void Move(Vector2 direction) => _movement.Move(direction * _speed * Time.deltaTime);
+    private void SetNullableFields()
+    {
+        _movement ??= GetComponent<Movement>();
+        _turnHandler ??= new TurnHandler(_renderer);
+    }
+    
+    public void Move(Vector2 direction)
+    {
+        _movement.Move(direction * _speed * Time.deltaTime);
+
+        CheckSide(direction);
+    }
+
+    private void CheckSide(Vector2 direction)
+    {
+        if (direction.x > 0) _turnHandler.ChangeSide(TurnHandler.PlayerSides.Right);
+        if (direction.x < 0) _turnHandler.ChangeSide(TurnHandler.PlayerSides.Left);
+    }
 }
---
+++ Assets/Scripts/Player/TurnHandler.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+namespace Player
+{
+    public class TurnHandler
+    {
+        private SpriteRenderer _renderer;
+        public TurnHandler(SpriteRenderer renderer) { _renderer = renderer; }
+
+        public void ChangeSide(PlayerSides side)
+        {
+            switch (side)
+            {
+                case PlayerSides.Left:
+                    RotateObj(_renderer.transform, 0);
+                    break;
+                default:
+                    RotateObj(_renderer.transform, 180);
+                    break;
+            }
+        }
+
+        private void RotateObj(Transform obj, float angle) => obj.localRotation = Quaternion.Euler(0f, angle, 0f);
+
+        public enum PlayerSides
+        {
+            Left,
+            Right
+        }
+    }
+}
