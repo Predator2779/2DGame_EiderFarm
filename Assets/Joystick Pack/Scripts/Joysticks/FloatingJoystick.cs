f25e8ff2c5adcd7c9c0614b2aeb5f77c73f813cd
Predator2779 <predator2779@gmail.com>
2023-11-15 01:55:01 +0500

    added CanvasGroup

--- Assets/Joystick Pack/Scripts/Joysticks/FloatingJoystick.cs
+++ Assets/Joystick Pack/Scripts/Joysticks/FloatingJoystick.cs
@@ -1,21 +1,27 @@
-﻿using UnityEngine.EventSystems;
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class FloatingJoystick : MyJoystick
 {
+    [SerializeField] private CanvasGroup _canvasGroup;
+    
     protected override void Start()
     {
+        _canvasGroup = transform.parent.GetComponent<CanvasGroup>();
         base.Start();
         background.gameObject.SetActive(false);
     }
 
     public override void OnEndDrag(PointerEventData eventData)
     {
+        _canvasGroup.blocksRaycasts = false;
         background.gameObject.SetActive(false);
         base.OnEndDrag(eventData);
     }
 
     public override void OnBeginDrag(PointerEventData eventData)
     {
+        _canvasGroup.blocksRaycasts = true;
         background.anchoredPosition = ScreenPointToAnchoredPosition(eventData.position);
         background.gameObject.SetActive(true);
         base.OnBeginDrag(eventData);
