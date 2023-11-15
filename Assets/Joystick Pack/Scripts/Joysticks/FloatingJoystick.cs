6b9e546b75df512eb98b18c554352a3a8613b4b7
Predator2779 <predator2779@gmail.com>
2023-11-16 01:34:54 +0500

    fixed begin drag
    теперь при первом таче стик появляется сразу

--- Assets/Joystick Pack/Scripts/Joysticks/FloatingJoystick.cs
+++ Assets/Joystick Pack/Scripts/Joysticks/FloatingJoystick.cs
@@ -8,16 +8,16 @@ public class FloatingJoystick : MyJoystick
         background.gameObject.SetActive(false);
     }
 
+    public override void OnBeginDrag(PointerEventData eventData)
+    {
+        base.OnBeginDrag(eventData);
+        background.anchoredPosition = ScreenPointToAnchoredPosition(eventData.position);
+        background.gameObject.SetActive(true);
+    }
+
     public override void OnEndDrag(PointerEventData eventData)
     {
         background.gameObject.SetActive(false);
         base.OnEndDrag(eventData);
-    }
-
-    public override void OnBeginDrag(PointerEventData eventData)
-    {
-        background.anchoredPosition = ScreenPointToAnchoredPosition(eventData.position);
-        background.gameObject.SetActive(true);
-        base.OnBeginDrag(eventData);
     }
 }
