7ed793b1da1c30fb0e4d37f67664ec6271e70e3c
Predator2779 <predator2779@gmail.com>
2023-11-15 21:27:00 +0500

    added bag-drawer

--- Assets/TestInventory.cs
+++ Assets/TestInventory.cs
@@ -3,10 +3,14 @@ using UnityEngine;
 
 public class TestInventory : MonoBehaviour
 {
+    [SerializeField] private BagContent _content;
+    [SerializeField] private int _minValue;
+    [SerializeField] private int _maxValue;
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.TryGetComponent(out Inventory _inventory))
             if (_inventory.TryGetBag(BagContent.Money, out Bag bag))
-                bag.AddPoints(Random.Range(10, 50));
+                bag.AddPoints(Random.Range(_minValue, _maxValue));
     }
 }
