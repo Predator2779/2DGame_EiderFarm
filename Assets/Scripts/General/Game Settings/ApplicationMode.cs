6345bccc172cec04345d9e71613274eea86fd788
Predator2779 <predator2779@gmail.com>
2023-11-16 02:04:41 +0500

    added menu and quit

---
+++ Assets/Scripts/General/Game Settings/ApplicationMode.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class ApplicationMode : MonoBehaviour
+{
+    [SerializeField] private GameObject _menuPanel;
+
+    public void SwitchMode() => _menuPanel.SetActive(!_menuPanel.activeSelf);
+
+    public void Quit() => Application.Quit();
+}
