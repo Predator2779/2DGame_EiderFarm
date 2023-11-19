41635b906aa229f90e1d41ff7041ea617961a4fc
Daniil Naumets <naumetsdaniil@mail.ru>
2023-11-19 16:44:41 +0300

    fixed transmitter

--- Assets/Machine.cs
+++ Assets/Machine.cs
@@ -7,11 +7,16 @@ using UnityEngine;
 [RequireComponent(typeof(BuildStorage))]
 public class Machine : MonoBehaviour
 {
-    [SerializeField] private BuildStorage _storage;
-    [SerializeField] private Converter _converter;
-    [SerializeField] private ResourceTransmitter _transmitter;
+    private BuildStorage _storage;
+    private Converter _converter;
+    private ResourceTransmitter _transmitter;
 
     private bool _isWorked;
+
+    [SerializeField, Header("Время изготовления")]
+    private int _delayProduction;
+
+    private ItemType _typeFromPlayer;
 
     private void Start()
     {
@@ -21,11 +26,6 @@ public class Machine : MonoBehaviour
 
         _transmitter.TransmitteEvent += Production;
     }
-
-    [SerializeField, Header("Время изготовления")]
-    private int _delayProduction;
-
-    private ItemType _typeFromPlayer;
 
     private void Make(int _fluffCount)
     {
