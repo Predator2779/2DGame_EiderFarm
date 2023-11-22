4e650459974462f2e81a1c7370c998742ee5c1bd
Predator2779 <predator2779@gmail.com>
2023-11-23 02:17:04 +0500

    added player animation

--- Assets/Scripts/Characters/Person.cs
+++ Assets/Scripts/Characters/Person.cs
@@ -1,4 +1,3 @@
-using System;
 using Player;
 using UnityEngine;
 
@@ -8,10 +7,7 @@ public class Person : MonoBehaviour, IMovable
     [Header("Character speed.")]
     [SerializeField, Range(0, 10)] private float _speed;
 
-    [SerializeField] private SpriteRenderer _renderer;
-
     private Movement _movement;
-    private TurnHandler _turnHandler;
 
     private void OnValidate() => SetNullableFields();
     private void Start() => SetNullableFields();
@@ -19,19 +15,10 @@ public class Person : MonoBehaviour, IMovable
     private void SetNullableFields()
     {
         _movement ??= GetComponent<Movement>();
-        _turnHandler ??= new TurnHandler(_renderer);
     }
-    
+
     public void Move(Vector2 direction)
     {
         _movement.Move(direction * _speed * Time.deltaTime);
-
-        CheckSide(direction);
-    }
-
-    private void CheckSide(Vector2 direction)
-    {
-        if (direction.x > 0) _turnHandler.ChangeSide(TurnHandler.PlayerSides.Right);
-        if (direction.x < 0) _turnHandler.ChangeSide(TurnHandler.PlayerSides.Left);
     }
 }
