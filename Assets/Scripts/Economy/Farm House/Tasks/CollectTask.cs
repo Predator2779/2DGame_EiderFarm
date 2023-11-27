e9893a9dffa09ccd1a497f6669e2d158283b6e6a
Predator2779 <66646630+Predator2779@users.noreply.github.com>
2023-11-27 17:59:07 +0500

    changed tasks

--- Assets/Scripts/Economy/Farm House/Tasks/CollectTask.cs
+++ Assets/Scripts/Economy/Farm House/Tasks/CollectTask.cs
@@ -1,45 +1,15 @@
-using General;
 using UnityEngine;
 
 namespace Economy.Farm_House
 {
-    [CreateAssetMenu(menuName = "Tasks/CollectTask", fileName = "New CollectTask", order = 0)]
-    public class CollectTask : Task
+    public abstract class CollectTask : Task
     {
-        [SerializeField] private Item _requiredItem;
+        [SerializeField] protected Item _requiredItem;
         [SerializeField] protected int _requireCount;
         [SerializeField] protected int _currentCount;
 
-        protected override void Initialize()
-        {
-            EventHandler.OnItemPickUp.AddListener(PickUpItem);
-            EventHandler.OnItemPut.AddListener(PutItem);
-        }
-
-        protected override void Deinitialize()
-        {
-            EventHandler.OnItemPickUp.RemoveListener(PickUpItem);
-            EventHandler.OnItemPut.RemoveListener(PutItem);
-        }
-
         public int GetRequireCount() => _requireCount;
         public int GetCurrentCount() => _currentCount;
-
-        private void PickUpItem(Item item, int count)
-        {
-            if (_requiredItem == item) 
-                _currentCount += count;
-            
-            CheckProgressing();
-        }
-
-        private void PutItem(Item item, int count)
-        {
-            if (_requiredItem == item) 
-                _currentCount -= count;
-            
-            CheckProgressing();
-        }
 
         protected override bool SomeCondition() => _currentCount >= _requireCount;
 
