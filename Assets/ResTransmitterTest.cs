ea60b0a285e1b19a4a61c2fd6c9f4b55df4c1015
Predator2779 <66646630+Predator2779@users.noreply.github.com>
2023-11-30 02:42:44 +0500

    added storage

---
+++ Assets/ResTransmitterTest.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using Building.Constructions;
+using Economy;
+using UnityEngine;
+
+[RequireComponent(typeof(Construction))]
+[RequireComponent(typeof(Inventory))]
+public class ResTransmitterTest : MonoBehaviour
+{
+    public delegate IEnumerator CoroutineDelegate(Item typeFrom, Inventory inv, int fluff);
+
+    public event CoroutineDelegate TransmitteEvent;
+
+    [SerializeField, Header("Сколько пуха передается от игрока")] private int _fluffCount;
+    [SerializeField] private Item _typeToPlayer;
+
+    private Inventory _characterInventory;
+    private Construction _construction;
+    private Inventory _inventory;
+
+    private void Start()
+    {
+        _construction = GetComponent<Construction>();
+        _inventory = GetComponent<Inventory>();
+    }
+
+    public void CheckBag()
+    {
+        if (_characterInventory == null) return;
+
+        Transmitte();
+
+        if (_fluffCount != 0 && TransmitteEvent != null)
+            StartCoroutine(TransmitteEvent?.Invoke(_typeToPlayer, _characterInventory, _fluffCount));
+    }
+
+    private void Transmitte()
+    {
+        var resources = _inventory.GetAllItems();
+        _characterInventory.AddItemsWithMsg(resources.ToArray(), _construction);
+        _inventory.ResetInventory();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<InputHandler>())
+        {
+            _characterInventory = collision.gameObject.GetComponent<Inventory>();
+            CheckBag();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (_characterInventory == collision.GetComponent<Inventory>() &&
+            collision.gameObject.GetComponent<InputHandler>())
+            _characterInventory = null;
+    }
+}
--- Assets/Scripts/Economy/Inventory.cs
+++ Assets/Scripts/Economy/Inventory.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using Building.Constructions;
 using General;
 using UnityEngine;
 using EventHandler = General.EventHandler;
@@ -19,6 +20,25 @@ namespace Economy
 
         public void AddItems(Item item, int count) => AddOrCreate(item, count);
 
+        public void AddItems(ItemBunch[] bunches)
+        {
+            foreach (var bunch in bunches)
+                AddOrCreate(bunch.GetItem(), bunch.GetCount());
+        }
+
+        public void AddItemsWithMsg(ItemBunch[] bunches, Construction construction)
+        {
+            foreach (var bunch in bunches)
+            {
+                Item item = bunch.GetItem();
+                int count = bunch.GetCount();
+                
+                AddOrCreate(item, count);
+                SendTransmitteMsg(construction,item, count);
+            }
+        }
+
+
         public void RemoveItems(Item item, int count)
         {
             if (IsExistsItems(item.GetName(), count)) Remove(item, count);
@@ -29,7 +49,7 @@ namespace Economy
         public bool IsExistsItems(string name, int count)
         {
             if (count < 0) return false;
-            
+
             return _listItems.Any(bunch => bunch.GetItemName() ==
                     name && bunch.GetCount() >= count);
         }
@@ -49,7 +69,7 @@ namespace Economy
         private void AddOrCreate(Item item, int count)
         {
             var bunch = GetBunch(item);
-            
+
             bunch.AddItems(count);
             SendCountItemsMsg(item.GetName(), bunch.GetCount());
             SendCountAddedMsg(item, count);
@@ -82,6 +102,8 @@ namespace Economy
             if (bunch.GetCount() <= 0) _listItems.Remove(bunch);
         }
 
+        public void ResetInventory() => _listItems = null;
+
         private void SendCountItemsMsg(string name, int count)
         {
             if (!_isPlayerInventory) return;
@@ -93,6 +115,11 @@ namespace Economy
         private void SendCountAddedMsg(Item item, int count)
         {
             if (_isPlayerInventory) EventHandler.OnItemPickUp?.Invoke(item, count);
+        }
+
+        private void SendTransmitteMsg(Construction construction, Item item, int count)
+        {
+            if (_isPlayerInventory) EventHandler.OnItemTransmitted?.Invoke(construction.typeConstruction, item, count);
         }
 
         private void SendCountRemovedMsg(Item item, int count)
