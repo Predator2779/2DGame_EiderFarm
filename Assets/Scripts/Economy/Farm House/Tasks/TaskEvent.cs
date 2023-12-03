6e189da1d337f037c900afc252db184908368ef1
Predator2779 <66646630+Predator2779@users.noreply.github.com>
2023-12-03 18:48:02 +0500

    fixed TaskEvent

--- Assets/Scripts/Economy/Farm House/Tasks/TaskEvent.cs
+++ Assets/Scripts/Economy/Farm House/Tasks/TaskEvent.cs
@@ -6,22 +6,13 @@ using EventHandler = General.EventHandler;
 public class TaskEvent : MonoBehaviour
 {
     [SerializeField] private Task _task;
-    public UnityEvent myEvent;
+    [SerializeField] private UnityEvent _myEvent;
 
-    private void Start()
-    {
-        myEvent?.Invoke();
-        EventHandler.OnTaskStageChanged.AddListener(CheckTask);
-    }
+    private void Start() => EventHandler.OnTaskStageChanged.AddListener(CheckTask);
 
     private void CheckTask(Task task, TaskStage stage)
     {
-        if (task == _task && stage == TaskStage.Completed)
-            myEvent?.Invoke();
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        myEvent?.Invoke();
+        if (task == _task && _task.GetStage() == TaskStage.Passed)
+            _myEvent?.Invoke();
     }
 }
