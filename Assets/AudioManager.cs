2c90bb4014c9f0ed7bfb02b47e863c553c7c232e
Daniil Naumets <naumetsdaniil@mail.ru>
2023-12-11 22:22:11 +0300

    add switches in sounds

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] GameObject _audio;
+
+    [SerializeField] Sprite[] _sprites;
+
+    [SerializeField] Image _buttonImage;
+
+    public void SwitchAudioMod()
+    {
+        if (_buttonImage.sprite == _sprites[0])
+        {
+            _buttonImage.sprite = _sprites[1];
+            _audio.SetActive(false);
+        }
+        else
+        {
+            _buttonImage.sprite = _sprites[0];
+            _audio.SetActive(true);
+        }
+    }
+}
