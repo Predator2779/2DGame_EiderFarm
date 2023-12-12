091ad0bc39f84b0012f4f47d30be014ea8b466e2
Predator2779 <66646630+Predator2779@users.noreply.github.com>
2023-12-13 00:49:02 +0500

    fixed cutscene loading

--- Assets/Scripts/General/Game Settings/ApplicationMode.cs
+++ Assets/Scripts/General/Game Settings/ApplicationMode.cs
@@ -1,14 +1,10 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class ApplicationMode : MonoBehaviour
 {
     [SerializeField] private GameObject _menuPanel;
-    [SerializeField] private AudioSource _audioSource;
 
     public void SwitchMode() => _menuPanel.SetActive(!_menuPanel.activeSelf);
-
-    public void ChangeVolume(Slider slider) => _audioSource.volume = slider.value;
     
     public void Quit() => Application.Quit();
 }
--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -3,12 +3,16 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    [SerializeField] private string _loadedScene;
+    [SerializeField] private string _cutScene;
+    
     private static bool _isNewGame;
 
     public void StartLevel(bool isNewGame)
     {
         _isNewGame = isNewGame;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+        SceneManager.LoadScene(isNewGame ? _cutScene : _loadedScene);
     }
 
     public void ExitLevel() => SceneManager.LoadScene(1);
