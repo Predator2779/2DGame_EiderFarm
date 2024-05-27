fecb2a6dfc3648da64cff55131d9d372534e1a8e
Predator2779 <66646630+Predator2779@users.noreply.github.com>
2024-05-28 00:30:56 +0500

    Update EmployeeAI.cs

--- Assets/Scripts/Characters/AI/EmployeeAI.cs
+++ Assets/Scripts/Characters/AI/EmployeeAI.cs
@@ -6,6 +6,7 @@ using General;
 using Other;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Serialization;
 
 namespace Characters.AI
 {
@@ -20,10 +21,12 @@ namespace Characters.AI
         [Space] [Header("Settings:")]
         [SerializeField] [Range(1, 100)] private int _fluffCapacity;
 
-        [SerializeField] private float _radius;
+        [FormerlySerializedAs("_distanceToTarget")] [FormerlySerializedAs("_radius")] [SerializeField]
+        private float _requireDistance;
 
         [Space] [Header("Thinking:")]
         [SerializeField] private SpriteRenderer _mindCloud;
+
         [SerializeField] private SpriteRenderer _thought;
         [SerializeField] private Thought[] _thoughts;
 
@@ -32,47 +35,65 @@ namespace Characters.AI
         private BuildStorage _currentStorage;
         private BuildingsPull _pull;
         private Employee _employee;
-        private Vector2 _target;
+        private Transform _target;
 
         private void Start() => Initialize();
         private void OnValidate() => Initialize();
-        private void FixedUpdate() => StateExecute();
+        private void FixedUpdate() => Execute();
 
         private void Initialize()
         {
             _pull ??= FindObjectOfType<BuildingsPull>();
             _employee ??= GetComponent<Employee>();
+            _currentEmployeeState = EmployeeStates.Idle;
+        }
+
+        private void Execute()
+        {
+            if (!IsDestination()) Walk();
+            else StateExecute();
         }
 
         private void CheckConditions()
         {
-            if (CountUncleanFluff() < _fluffCapacity &&
-                CountCleanFluff() <= 0 &&
-                CanPickFluff())
+            if (CanPick())
             {
                 SetTarget(_currentHouse.gameObject);
                 _currentEmployeeState = EmployeeStates.Picking;
                 return;
             }
 
-            if (CountCleanFluff() < CountUncleanFluff() &&
-                CountUncleanFluff() > 0 &&
-                CanRecycle())
+            if (CanRecycle())
             {
                 SetTarget(_currentCleaner.gameObject);
                 _currentEmployeeState = EmployeeStates.Recycling;
                 return;
             }
 
-            if (CountCleanFluff() > 0 && CanTransportable())
+            if (CanTransportation())
             {
                 SetTarget(_currentStorage.gameObject);
                 _currentEmployeeState = EmployeeStates.Transportation;
                 return;
             }
 
-            _currentEmployeeState = EmployeeStates.Patrol;
+            _currentEmployeeState = EmployeeStates.Idle;
         }
+
+        // собирает, пока не заполнится до максимума и при этом нет очищенного
+        private bool CanPick() => CountUncleanFluff() < _fluffCapacity &&
+                                  CountCleanFluff() <= 0 &&
+                                  HasUncleanFluff();
+
+        // обрабатывает, пока чистый не заполнится до максимума, либо пока не кончится неочищенный
+        private bool CanRecycle() => (CountCleanFluff() < _fluffCapacity ||
+                                      CountUncleanFluff() > 0) &&
+                                     HasCleaner();
+
+        // стоит у склада, пока не выгрузит весь очищенный пух
+        private bool CanTransportation() => CountCleanFluff() > 0 &&
+                                            HasStorage();
+
 
         private void StateExecute()
         {
@@ -82,38 +103,24 @@ namespace Characters.AI
                     Idle();
                     break;
                 case EmployeeStates.Picking:
-                    ChangeMindCloud(_currentEmployeeState);
                     Picking();
                     break;
                 case EmployeeStates.Recycling:
-                    ChangeMindCloud(_currentEmployeeState);
                     Recycling();
                     break;
                 case EmployeeStates.Transportation:
-                    ChangeMindCloud(_currentEmployeeState);
                     Transportation();
                     break;
-                case EmployeeStates.Patrol:
-                    Idle();
-                    break;
-                case EmployeeStates.Walk:
-                    Walk();
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
             }
         }
 
         private void ChangeMindCloud(EmployeeStates state)
         {
-            print("base state: " + state);
             _mindCloud.gameObject.SetActive(true);
 
             foreach (var t in _thoughts)
-            {
-                print(t.state);
-                if (t.state == state) _thought.sprite = t.sprite;
-            }
+                if (t.state == state)
+                    _thought.sprite = t.sprite;
         }
 
         private void Idle()
@@ -123,88 +130,36 @@ namespace Characters.AI
             CheckConditions();
         }
 
-        private void Walk()
-        {
-            if (IsNearby(_target))
-            {
-                _currentEmployeeState = EmployeeStates.Idle;
-                return;
-            }
-
-            base.WalkAnimation(_target - (Vector2) transform.position);
-        }
+        private void Walk() => base.WalkAnimation(_target.position - transform.position);
 
         private void Picking()
         {
             if (_currentHouse == null ||
-                _currentHouse.GetFluffCount() <= 0 ||
-                CountUncleanFluff() > _fluffCapacity)
-            {
-                CheckConditions();
-                return;
-            }
-
-            WalkToTarget();
+                _currentHouse.GetFluffCount() <= 0
+                    // || CountUncleanFluff() > _fluffCapacity
+            )
+                _currentEmployeeState = EmployeeStates.Idle;
+            print("собираю...");
         }
 
         private void Recycling()
         {
             if (_currentCleaner == null ||
-                CountCleanFluff() >= _fluffCapacity)
-            {
-                CheckConditions();
-                return;
-            }
-
-            WalkToTarget();
+                CountUncleanFluff() <= 0)
+                _currentEmployeeState = EmployeeStates.Idle;
+            print("обрабатываю...");
         }
 
         private void Transportation()
         {
             if (_currentStorage == null ||
                 CountCleanFluff() <= 0)
-            {
-                CheckConditions();
-                return;
-            }
-
-            WalkToTarget();
+                _currentEmployeeState = EmployeeStates.Idle;
+            print("транспортирую...");
         }
 
-        private bool IsNearby(Vector2 target)
-        {
-            var distance = Vector2.Distance(transform.position, target);
-            return distance <= _radius * 1.5f;
-            ;
-        }
-
-        private void WalkToTarget()
-        {
-            _agent.SetDestination(_target);
-            _currentEmployeeState = EmployeeStates.Walk;
-        }
-
-        private void StopMove()
-        {
-            _personAnimate.Walk(_target, false);
-            StopSound();
-        }
-
-        private void SetTarget(GameObject target)
-        {
-            if (target.TryGetComponent(out Construction construction))
-            {
-                var entryPoint = construction.GetEntryPoint();
-
-                if (entryPoint != null)
-                {
-                    _target = entryPoint.position;
-                    return;
-                }
-            }
-
-            _target = target.transform.position;
-        }
+        private bool IsDestination() =>
+                !(_target != null && Vector2.Distance(transform.position, _target.position) > _requireDistance);
 
         private int CountUncleanFluff() => TryGetBunch(GlobalConstants.UncleanedFluff)?.GetCount() ?? 0;
         private int CountCleanFluff() => TryGetBunch(GlobalConstants.CleanedFluff)?.GetCount() ?? 0;
@@ -214,7 +169,34 @@ namespace Characters.AI
                 ? bunch
                 : null;
 
-        private bool CanPickFluff()
+        private void StopMove()
+        {
+            var direction = Vector2.zero;
+
+            if (_target) direction = _target.position;
+            
+            _personAnimate.Walk(direction, false);
+            StopSound();
+        }
+
+        private void SetTarget(GameObject target)
+        {
+            if (target.TryGetComponent(out Construction construction))
+            {
+                var entryPoint = construction.GetEntryPoint();
+                if (entryPoint != null)
+                {
+                    _target = entryPoint;
+                    return;
+                }
+            }
+
+            _target = target.transform;
+            _agent.SetDestination(_target.position);
+            ChangeMindCloud(_currentEmployeeState);
+        }
+
+        private bool HasUncleanFluff()
         {
             var gagaHouses = _pull.GagaHouses;
 
@@ -222,38 +204,38 @@ namespace Characters.AI
             {
                 var bMenu = house.GetBuildMenu();
 
-                if (!bMenu.IsBuilded ||
-                    !bMenu.GetBuilding().TryGetComponent(out BuildStorage storage) ||
-                    house.IsOccupied() ||
-                    storage.GetFluffCount() <= 0) continue;
-
-                _currentHouse = storage;
-                return true;
+                if (bMenu.IsBuilded &&
+                    bMenu.GetBuilding().TryGetComponent(out BuildStorage storage) &&
+                    !house.IsOccupied() &&
+                    storage.GetFluffCount() > 0)
+                {
+                    _currentHouse = storage;
+                    return true;
+                }
             }
 
             return false;
         }
 
-        private bool CanRecycle()
+        private bool HasCleaner()
         {
             var cleaners = _pull.Cleaners;
 
             foreach (var cleaner in cleaners)
             {
-                if (!cleaner.GetBuildMenu().IsBuilded ||
-                    cleaner.IsOccupied() ||
-                    cleaner.GetBuildMenu().GetBuilding().typeConstruction != GlobalTypes.TypeBuildings.FluffCleaner
-                    || CountUncleanFluff() < 2)
-                    continue;
-
-                _currentCleaner = cleaner.GetBuildMenu().GetBuilding();
-                return true;
+                if (cleaner.GetBuildMenu().IsBuilded &&
+                    !cleaner.IsOccupied() &&
+                    cleaner.GetBuildMenu().GetBuilding().typeConstruction == GlobalTypes.TypeBuildings.FluffCleaner)
+                {
+                    _currentCleaner = cleaner.GetBuildMenu().GetBuilding();
+                    return true;
+                }
             }
 
             return false;
         }
 
-        private bool CanTransportable()
+        private bool HasStorage()
         {
             var storages = _pull.Storages;
 
@@ -261,13 +243,13 @@ namespace Characters.AI
             {
                 var bMenu = storage.GetBuildMenu();
 
-                if (!bMenu.IsBuilded ||
-                    storage.IsOccupied() ||
-                    !bMenu.GetBuilding().TryGetComponent(out BuildStorage bStorage))
-                    continue;
-
-                _currentStorage = bStorage;
-                return true;
+                if (bMenu.IsBuilded &&
+                    !storage.IsOccupied() &&
+                    bMenu.GetBuilding().TryGetComponent(out BuildStorage bStorage))
+                {
+                    _currentStorage = bStorage;
+                    return true;
+                }
             }
 
             return false;
@@ -278,7 +260,7 @@ namespace Characters.AI
         private void OnDrawGizmos()
         {
             Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(transform.position, _radius);
+            Gizmos.DrawWireSphere(transform.position, _requireDistance);
         }
 
 #endif
@@ -288,9 +270,7 @@ namespace Characters.AI
             Idle,
             Picking,
             Recycling,
-            Transportation,
-            Patrol,
-            Walk
+            Transportation
         }
     }
 }
