6c17c91597cf8055d1dd0665195742cbb869eff2
Predator2779 <66646630+Predator2779@users.noreply.github.com>
2024-06-08 14:28:57 +0500

    fixed prefabs

--- Assets/Scripts/General/Spawner.cs
+++ Assets/Scripts/General/Spawner.cs
@@ -9,7 +9,6 @@ namespace General
         [SerializeField] private GameObject[] _spawnObjs;
         [SerializeField] private int _countObjs;
         [SerializeField] private bool _isRandomPos;
-        [SerializeField] private CircleCollider2D _circle;
         [SerializeField] private float _radius;
 
         public void Spawn()
@@ -21,18 +20,14 @@ namespace General
                     Instantiate(_spawnObjs[i], GetPosition(), Quaternion.identity);
         }
 
-        private float GetRadius() => _circle != null ? GetRadiusCircle() : _radius;
-        private float GetRadiusCircle() => _circle.radius * _circle.transform.localScale.x;
-
         private Vector2 GetPosition()
         {
             Vector2 currentPos = transform.position;
             Vector2 position = currentPos;
 
             if (!_isRandomPos) return position;
-
-            float radius = GetRadius();
-            position = currentPos + Random.insideUnitCircle * radius;
+            
+            position = currentPos + Random.insideUnitCircle * _radius;
 
             return position;
         }
@@ -42,7 +37,7 @@ namespace General
         private void OnDrawGizmos()
         {
             Gizmos.color = Color.blue;
-            Gizmos.DrawWireSphere(transform.position, GetRadius());
+            Gizmos.DrawWireSphere(transform.position, _radius);
         }
 
 #endif
